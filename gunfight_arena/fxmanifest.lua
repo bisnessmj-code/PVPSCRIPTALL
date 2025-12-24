4e4b4d5272c5a85662593c21e592724f59e0df6d
bisnessmj-code <bisness.mj@gmail.com>
2025-12-24 17:45:43 +0100

    save

--- gunfight_arena/fxmanifest.lua
+++ gunfight_arena/fxmanifest.lua
@@ -1,4 +1,3 @@
-shared_script '@WaveShield/resource/include.lua'
 
 fx_version 'cerulean'
 game 'gta5'
@@ -69,6 +68,8 @@ files {
     'html/images/zone8.webp',
     'html/images/zone9.webp',
     'html/images/zone10.webp',
+    -- Logo (peut rester en PNG ou passer en WebP)
+    'html/images/logo.wep',
     -- Image par défaut
     'html/images/default.webp'
 }
